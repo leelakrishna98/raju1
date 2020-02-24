45d0b3bf4c9438622ec2ad72068a016e281876b0
BVRIT <BVRIT@PLACEMENT-LAB-35>
2020-02-24 09:32:38 +0530

    raju project

---
+++ raju/src/main/java/bvrit/raju/App.java
@@ -0,0 +1,13 @@
+package bvrit.raju;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
